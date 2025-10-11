9ce3342c17a968d00826b572846a2cfdbf7681ff
Brunobonfimdev <bbonfim611@gmail.com>
2025-10-11 00:53:14 -0300

    segurança, remover credenciais do código

--- enviar_recuperacao.php
+++ enviar_recuperacao.php
@@ -3,6 +3,9 @@
 require_once 'conexao.php';
 require_once 'funcoes.php';
 
+// **** INCLUI O ARQUIVO DE CONFIGURAÇÃO COM AS SENHAS ****
+require_once 'config.php';
+
 // Inclui os arquivos do PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -24,36 +27,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $usuario = $stmt->fetch();
 
     if ($usuario) {
+        // ... (lógica de token continua a mesma) ...
         $token = bin2hex(random_bytes(32));
         $token_hash = hash('sha256', $token);
         $expira_em = date("Y-m-d H:i:s", time() + 3600);
-
         $stmt_update = $conn->prepare("UPDATE Usuario SET reset_token_hash = ?, reset_token_expires_at = ? WHERE id = ?");
+
         if ($stmt_update->execute([$token_hash, $expira_em, $usuario['id']])) {
-            
             $link_recuperacao = "http://localhost/financeiro/redefinir_senha.php?token=" . $token;
 
             $mail = new PHPMailer(true);
             try {
-                
+                // Configurações do Servidor
                 $mail->isSMTP();
                 $mail->Host       = 'smtp.gmail.com';
                 $mail->SMTPAuth   = true;
-                $mail->Username   = 'xxxx'; 
-                $mail->Password   = 'xxxx';   
+                
+                // **** USA AS CONSTANTES DO ARQUIVO config.php ****
+                $mail->Username   = SMTP_USER; 
+                $mail->Password   = SMTP_PASS; 
+                
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                 $mail->Port       = 587;
                 $mail->CharSet    = 'UTF-8';
 
                 // Remetente e Destinatário
-                $mail->setFrom('brunowt360@gmail.com', 'Sistema Financeiro');
+                $mail->setFrom(SMTP_USER, 'Sistema Financeiro');
                 $mail->addAddress($usuario['email'], $usuario['nome']);
 
                 // Conteúdo do Email
                 $mail->isHTML(true);
                 $mail->Subject = 'Recuperação de Senha - Sistema Financeiro';
-                $mail->Body    = "Olá, {$usuario['nome']}.<br><br>Recebemos uma solicitação para redefinir sua senha. Clique no link abaixo para criar uma nova senha:<br><br><a href='{$link_recuperacao}'>Redefinir Minha Senha</a><br><br>Se você não solicitou isso, pode ignorar este email.<br><br>Atenciosamente,<br>Equipe do Sistema Financeiro";
-                $mail->AltBody = "Olá, {$usuario['nome']}.\n\nPara redefinir sua senha, copie e cole o seguinte link no seu navegador:\n{$link_recuperacao}\n\nSe você não solicitou isso, pode ignorar este email.";
+                $mail->Body    = "Olá, {$usuario['nome']}.<br><br>... (conteúdo do email) ...";
+                $mail->AltBody = "Olá, {$usuario['nome']}.\n\n... (conteúdo do email) ...";
 
                 $mail->send();
                 $_SESSION['mensagem'] = "Um link para redefinição de senha foi enviado para o seu email.";
